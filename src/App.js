c8d1b742ea804451e07260c66ac8e76073809c73
Rabin Chaudhary <nibar18@gmail.com>
2018-05-22 16:27:59 +1000

    Changed lists into dynamic lists

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ class App extends Component {
 //original state
   state = {
       persons: [
-        {name:'Rabin', age:20},
-        {name:'Ayush', age:40},
-        {name:'akjl', age:20},
+        {id:1,name:'Rabin', age:20},
+        {id:2,name:'Ayush', age:40},
+        {id:3,name:'akjl', age:20},
       ],
       showPersons:false
   }
 // clickEvent method to get dynamic name
-clickEventHandler =(newName)=>{
+/** clickEventHandler =(newName)=>{
 //setting up value
   this.setState({
     persons: [
@@ -27,22 +27,37 @@ clickEventHandler =(newName)=>{
   });
 
 }
+*/
 //To toggle
- personToggleHandler =()=>{
+ personToggleHandler =()=> {
    const valueCheck = this.state.showPersons;
    this.setState({ showPersons: !valueCheck });
 
  }
-//change name
-nameChangeHandler =(event)=>{
-  this.setState({
-    persons: [
-      {name:"Rabin", age:20},
-      {name:event.target.value, age:40},
-      {name:'akjl', age:20},
-    ]
-  });
 
+//change name
+nameChangeHandler =(event, id)=>{
+  //Try to find index of changing array object
+  const personIndex = this.state.persons.findIndex(p=>{return p.id === id});
+
+  //creating copy of persons array named as person and acessing value of personIndex
+    const person = {...this.state.persons[personIndex]};
+  //Converting matching id name into target name
+    person.name = event.target.value;
+  //creating new copy of array
+    const persons = [...this.state.persons];
+  //assaining changed value to array element index
+    persons[personIndex] = person;
+//updating state using setState function
+  this.setState({ persons: persons});
+
+}
+
+deletePersonHandler =(indexPerson)=>{
+  //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]
+        persons.splice(indexPerson,1);
+  this.setState({persons:persons});
 }
 
 /* React renders whenever it is necessary so it it better to put conditions in render rather than return */
@@ -52,17 +67,17 @@ nameChangeHandler =(event)=>{
     if(this.state.showPersons){
       persons =(
       <div>
-        <Person
-        name= {this.state.persons[0].name}
-        age={this.state.persons[0].age}> My Hobbies: This is my hobbies </Person>
+          {this.state.persons.map((person,index) =>{
+            return(
+              <Person
+              key={index}
+              name={person.name}
+              age={person.age}
+              click={this.deletePersonHandler.bind(this,index)}
+              changed={(event)=>this.nameChangeHandler(event,person.id)} />
+            )
 
-        <Person
-        name= {this.state.persons[1].name}
-        age={this.state.persons[1].age}
-        click={this.clickEventHandler.bind(this,"Rabin")}
-        changed ={this.nameChangeHandler} />
-
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> My habbits: </Person>
+          })}
 
       </div>);
     }
@@ -70,7 +85,7 @@ nameChangeHandler =(event)=>{
     return (
       <div className="App">
         <p> This is really awesome </p>
-        <button onClick={this.clickEventHandler.bind(this,"Arjun")}> Click Me </button>
+        {/*<button onClick={this.clickEventHandler.bind(this,"Arjun")}> Click Me </button> */}
         <button onClick={this.personToggleHandler}>Alternate Button </button>  {/* alternate way to bind using anynomous */}
         {persons}
       </div>
